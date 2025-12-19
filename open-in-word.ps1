8c98dbd38f84c2dd771d8b111671f583af9b97cc
bryanlharris <48188000+bryanlharris@users.noreply.github.com>
2025-12-19 07:02:08 -0500

    Set Word launch sleep to 2 seconds

--- open-in-word.ps1
+++ open-in-word.ps1
@@ -3,7 +3,7 @@ param(
 )
 
 Start-Process winword.exe
-Start-Sleep -Milliseconds 300
+Start-Sleep -Milliseconds 2000
 
 $word = [Runtime.InteropServices.Marshal]::GetActiveObject("Word.Application")
 $word.Documents.Open($File) | Out-Null
